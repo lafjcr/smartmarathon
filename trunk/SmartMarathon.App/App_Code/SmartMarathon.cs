3407d1b69614a2e0ed375297d383dc4118fff4fa
lafj14@gmail.com <lafj14@gmail.com@be496e2f-b671-9690-935e-8b4a7ee91420>
2014-08-06 16:22:39 +0000

    Implemented SmartMarathon view

---
+++ trunk/SmartMarathon.App/App_Code/SmartMarathon.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using SmartMarathon.App.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartMarathon.App.Code
+{
+    public class SmartMarathon
+    {
+        public static void LoadMarathons()
+        {
+            var json = File.ReadAllText(HttpContext.Current.Server.MapPath("~/marathons.json"));
+            var marathons = JsonConvert.DeserializeObject<List<MarathonData>>(json);
+            HttpContext.Current.Session["Marathons"] = marathons;
+        }
+
+        public static IEnumerable<SelectListItem> Marathons()
+        {
+            var marathons = HttpContext.Current.Session["Marathons"] as List<MarathonData>;
+
+            var result = new List<SelectListItem>();
+            marathons.ForEach(item => result.Add(new SelectListItem() { Value = String.Format("{0};{1};{2}", item.Id, item.Kms, item.Miles), Text = item.Name}));
+
+            return result;
+        }
+
+        public static IEnumerable<SelectListItem> SplitCategories()
+        {
+            var splitCategories = Enum.GetValues(typeof(SplitCategory)).Cast<SplitCategory>();
+            var splitCategoriesList = from splitCategory in splitCategories
+                                    select new SelectListItem
+                                    {
+                                        Text = splitCategory.ToString(),
+                                        Value = ((int)splitCategory).ToString()
+                                    };
+            return splitCategoriesList;
+        }
+    }
+}
