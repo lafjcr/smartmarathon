5f8db5ef610a147251ea8b4026590553a3f04a5a
lafj14@gmail.com <lafj14@gmail.com@be496e2f-b671-9690-935e-8b4a7ee91420>
2014-08-06 16:21:09 +0000

    Implemented SmartMarathon view

--- trunk/SmartMarathon.App/Controllers/HomeController.cs
+++ trunk/SmartMarathon.App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartMarathon.App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,18 @@ namespace SmartMarathon.App.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult SmartMarathon()
+        {
+            return View(new SmartMarathonData());
+        }
+
+        [HttpPost]
+        public ActionResult SmartMarathon(SmartMarathonData model)
+        {
+            model.Calculate();
+            return View(model);
         }
 
         public ActionResult About()
