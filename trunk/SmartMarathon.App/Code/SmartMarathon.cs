9b1b22be3cd5b9ce84f70d23532645a0b982132b
lafj14@gmail.com <lafj14@gmail.com@be496e2f-b671-9690-935e-8b4a7ee91420>
2014-09-16 03:51:30 +0000

    Order marathons by country and then by name

--- trunk/SmartMarathon.App/Code/SmartMarathon.cs
+++ trunk/SmartMarathon.App/Code/SmartMarathon.cs
@@ -23,8 +23,8 @@ namespace SmartMarathon.App.Code
             var marathons = HttpContext.Current.Session["Marathons"] as List<MarathonData>;
 
             var result = new List<SelectListItem>();
-            marathons = marathons.Where(item => item.Distance == distance).OrderBy(item => item.Name).ToList();
-            marathons.ForEach(item => result.Add(new SelectListItem() { Value = String.Format("{0};{1};{2}", item.Id, item.Kms, item.Miles), Text = item.Name, Selected = item.Id == 0}));            
+            marathons = marathons.Where(item => item.Distance == distance).OrderBy(item => item.Country).ThenBy(item => item.Name).ToList();
+            marathons.ForEach(item => result.Add(new SelectListItem() { Value = String.Format("{0};{1};{2}", item.Id, item.Kms, item.Miles), Text = String.Format("[{0}] {1}", item.Country, item.Name), Selected = item.Id == 0}));            
 
             return result;
         }
