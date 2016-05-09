ae327f1501146537197ed0ddd522fbfcffbcbfe8
Luis Fernandez <lafj14@gmail.com>
2016-05-09 13:43:52 -0600

    Hide About/Contact views

--- SmartMarathon.App/Controllers/HomeController.cs
+++ SmartMarathon.App/Controllers/HomeController.cs
@@ -55,14 +55,14 @@ namespace SmartMarathon.App.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
+        //public ActionResult About()
+        //{
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            return View();
-        }
+        //public ActionResult Contact()
+        //{
+        //    return View();
+        //}
     }
 }
